9b664b3e7281a23ab9da3d4b61e8c2af41656623
naekko-creator <ahmad.freelance92@gmail.com>
2026-01-18 19:33:50 +0800

    Add query for sales after January 6, 2024
    Added a query to retrieve sales data after January 6, 2024.

--- data-assistant-portfolio/01_data_cleaning_python/02_data_analysis_sql/queries.sql
+++ data-assistant-portfolio/01_data_cleaning_python/02_data_analysis_sql/queries.sql
@@ -6,4 +6,10 @@ SELECT
     SUM(quantity * price) AS total_revenue
 FROM sales_data
 GROUP BY product;
+-- Sales after January 6, 2024
+SELECT *
+FROM sales_data
+WHERE order_date > '2024-01-06';
+
+
 
