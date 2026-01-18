0265d27d8aee3b2475939a1a059ae28f8c333da4
naekko-creator <ahmad.freelance92@gmail.com>
2026-01-18 19:32:46 +0800

    Add query for total revenue per product

--- data-assistant-portfolio/01_data_cleaning_python/02_data_analysis_sql/queries.sql
+++ data-assistant-portfolio/01_data_cleaning_python/02_data_analysis_sql/queries.sql
@@ -1,2 +1,9 @@
 -- Show all sales data
 SELECT * FROM sales_data;
+-- Total revenue per product
+SELECT
+    product,
+    SUM(quantity * price) AS total_revenue
+FROM sales_data
+GROUP BY product;
+
